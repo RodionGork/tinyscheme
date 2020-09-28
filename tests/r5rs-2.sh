688d80f6b84587861faaf4703068e524b83d8959
rodion <rodiongork@github.com>
2020-09-28 19:38:27 +0300

    More tests from Chibi r5rs-tests

--- tests/r5rs-2.sh
+++ tests/r5rs-2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# remade from Chibi Scheme "r5rs-tests.scm"
+
 out=$(./scheme -c '(define let*-def 1) (let* () (define let*-def 2) #f) (display let*-def)')
 if [[ "$out" != "1" ]] ; then
   exit 1
@@ -21,3 +24,34 @@ if [[ "$out" != "((6 1 3) (-5 -2))" ]] ; then
   echo "OUT: $out"
   exit 1
 fi
+
+
+exit
+# the following fails, so let's exit here
+
+code=$(cat << STOP
+(display
+    (let ((path '())
+          (c #f))
+      (let ((add (lambda (s)
+                   (set! path (cons s path)))))
+        (dynamic-wind
+            (lambda () (add 'connect))
+            (lambda ()
+              (add (call-with-current-continuation
+                    (lambda (c0)
+                      (set! c c0)
+                      'talk1))))
+            (lambda () (add 'disconnect)))
+        (if (< (length path) 4)
+            (c 'talk2)
+            (reverse path)))))
+STOP
+)
+
+out=$(./scheme -c "$code")
+if [[ "$out" != "(connect talk1 disconnect connect talk2 disconnect)" ]] ; then
+  echo "OUT: $out"
+  exit 1
+fi
+
